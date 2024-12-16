0e45f2cc68eb0ede6d007771718b8596ddff7a4f
CpmJhon21 <cpmjho@gmail>
2024-12-16 10:23:24 +0700

    Welcome To Cpm_Jhon21

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // Redirect setelah 3 detik
+    setTimeout(() => {
+        window.location.href = "index2.html";
+    }, 3000); // 3000 ms = 3 detik
+});
